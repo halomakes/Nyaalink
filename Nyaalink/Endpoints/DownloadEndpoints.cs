63ca1f0fe03bf3346004e8aa4dd7b72361ba311c
halomademeapc <alex@halomademeapc.com>
2025-02-11 15:56:43 -0500

    add downloads by rule endpoint

--- Nyaalink/Endpoints/DownloadEndpoints.cs
+++ Nyaalink/Endpoints/DownloadEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Nyaalink.Endpoints;
@@ -7,13 +8,29 @@ public static class DownloadEndpoints
 {
     public static void MapDownloadEndpoints(this IEndpointRouteBuilder builder)
     {
-        builder.MapGet("api/downloads/recent", async ([FromServices] DownloadContext db, CancellationToken ct, [FromQuery] int limit = 100) =>
-        {
-            var results = await db.Records
-                .OrderByDescending(static r => r.IngestedAt)
-                .Take(limit)
-                .ToListAsync(ct);
-            return Results.Ok(results);
-        });
+        builder.MapGet("api/downloads/recent",
+                async ([FromServices] DownloadContext db, CancellationToken ct, [FromQuery] int limit = 100) =>
+                {
+                    var results = await db.Records
+                        .OrderByDescending(static r => r.IngestedAt)
+                        .Take(limit)
+                        .ToListAsync(ct);
+                    return Results.Ok(results);
+                })
+            .WithName("GetRecentDownloads")
+            .Produces<IList<DownloadRecord>>((int)HttpStatusCode.OK);
+
+        builder.MapGet("api/rules/{ruleId:int}/downloads", async ([FromServices] DownloadContext db,
+                [FromRouteAttribute] int ruleId, CancellationToken ct, [FromQuery] int limit = 100) =>
+            {
+                var results = await db.Records
+                    .Where(r => r.RuleId == ruleId)
+                    .OrderByDescending(static r => r.IngestedAt)
+                    .Take(limit)
+                    .ToListAsync(ct);
+                return Results.Ok(results);
+            })
+            .WithName("GetDownloadsByRuleId")
+            .Produces<IList<DownloadRecord>>((int)HttpStatusCode.OK);
     }
 }
